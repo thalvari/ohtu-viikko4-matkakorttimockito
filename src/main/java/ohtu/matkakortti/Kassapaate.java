507b60fe1bb50dda049f7353656d62d91decf8b0
thalvari <tuomas.halvari@helsinki.fi>
2018-11-25 05:17:16 +0200

    -

--- src/main/java/ohtu/matkakortti/Kassapaate.java
+++ src/main/java/ohtu/matkakortti/Kassapaate.java
@@ -1,23 +1,29 @@
 package ohtu.matkakortti;
 
 public class Kassapaate {
+
     private int myytyjaLounaita;
     public static final int HINTA = 5;
 
     public Kassapaate() {
         this.myytyjaLounaita = 0;
     }
-    
-    public void lataa(Matkakortti kortti, int summa){
-        kortti.lataa(summa);
+
+    public void lataa(Matkakortti kortti, int summa) {
+        if (summa > 0) {
+            kortti.lataa(summa);
+        }
     }
-    
+
     public void ostaLounas(Matkakortti kortti) {
-        kortti.osta(HINTA);
+        if (kortti.getSaldo() >= HINTA) {
+            kortti.osta(HINTA);
+        }
         myytyjaLounaita++;
     }
 
     public int getMyytyjaLounaita() {
         return myytyjaLounaita;
     }
+
 }
--- src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -1,33 +1,27 @@
-
 package ohtu.matkakortti;
 
-import ohtu.matkakortti.Matkakortti;
-import ohtu.matkakortti.Kassapaate;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class KassapaateTest {
-    
+
     Kassapaate kassa;
     Matkakortti kortti;
-    
+
     @Before
     public void setUp() {
         kassa = new Kassapaate();
         kortti = mock(Matkakortti.class);
     }
-    
+
     @Test
     public void kortiltaVelotetaanHintaJosRahaaOn() {
         when(kortti.getSaldo()).thenReturn(10);
         kassa.ostaLounas(kortti);
-        
         verify(kortti, times(1)).getSaldo();
         verify(kortti).osta(eq(Kassapaate.HINTA));
     }
@@ -36,9 +30,20 @@ public class KassapaateTest {
     public void kortiltaEiVelotetaJosRahaEiRiita() {
         when(kortti.getSaldo()).thenReturn(4);
         kassa.ostaLounas(kortti);
-        
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
-      
+
+    @Test
+    public void kortilleYritetäänLadataJosSummaPositiivinen() {
+        kassa.lataa(kortti, 1);
+        verify(kortti, times(1)).lataa(anyInt());
+    }
+
+    @Test
+    public void kortilleEiYritetäLadataJosSummaNegatiivinen() {
+        kassa.lataa(kortti, -1);
+        verify(kortti, times(0)).lataa(anyInt());
+    }
+
 }
